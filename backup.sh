f1e45f88e2c6d13ab836a79bf8b4547102bd539c
Yash Deokar <148381730+Yashdeoka@users.noreply.github.com>
2024-07-12 12:35:29 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+
+<< readme
+
+this scripts takes backup og given directory
+usage:
+./backup.sh <path of directory> <path of backup dir>
+readme
+
+source_dir=$1
+target_dir=$2
+timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+backup_dir="${target_dir}/backup_${timestamp}"
+
+function create_backup {
+
+
+zip -r "${backup_dir}.zip"  "${source_dir}" > /dev/null
+
+if [ $? -eq 0 ]; then
+        echo "Backup creted successfully"
+else
+        echo "backup was not performed for $timestamp"
+fi
+}
+
+function perform_rotation {
+        backups=($(ls -t "$target_dir/backup_"*.zip))
+
+        if [ "${#backups[@]}" -gt 5 ]; then
+                backups_to_remove=("${backups[@]:5}")
+                for backup in "${backups_to_remove[@]}";
+                do
+                        rm "$backup"
+
+                done
+        fi
+
+
+
+}
+
+create_backup
+
+perform_rotation
